fa473b84e832b055fcf920663603b2039d56524d
Moritz Ulrich <moritz@tarn-vedra.de>
2019-11-28 17:00:09 +0100

    Add (slow) test to check `$busy` implementation when the DB blocks

--- test/exqlite/connection_test.exs
+++ test/exqlite/connection_test.exs
@@ -1,5 +1,5 @@
 defmodule Exqlite.ConnectionTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
   doctest Exqlite.Connection
 
   alias Exqlite.Query
@@ -36,10 +36,45 @@ defmodule Exqlite.ConnectionTest do
     end)
   end
 
+  test "various sqltie functions" do
+    {:ok, db} = DBConnection.start_link(Exqlite.Connection, @opts)
+    assert {:ok, _query, [["sqlite_version()": _]]} = DBConnection.prepare_execute(db, Query.from("select sqlite_version()"), [])
+  end
+
   test "invalid sql syntax" do
     {:ok, db} = DBConnection.start_link(Exqlite.Connection, @opts)
     assert {:error, _} = DBConnection.prepare(db, Query.from("select asdf"), [])
     assert {:error, _} = DBConnection.execute(db, Query.from("select asdf"), [])
     assert {:error, _} = DBConnection.prepare_execute(db, Query.from("select asdf"), [])
+  end
+
+  test "blocking operations on the same database file" do
+    file = "db_#{:erlang.phash2(make_ref())}"
+    try do
+      {:ok, blocking_db} = DBConnection.start_link(Exqlite.Connection, [ database: file ])
+      create_table_query = Query.from("create table test (id integer)")
+      {:ok, _, _} = DBConnection.prepare_execute(blocking_db, create_table_query, [])
+
+      insert_query = Query.from("insert into test (id) values (42)")
+
+      {:ok, db} = DBConnection.start_link(Exqlite.Connection, [ database: file ])
+
+      parent = self()
+      {:ok, task} = Task.start_link(fn ->
+        DBConnection.transaction(blocking_db, fn blocking_db ->
+          assert {:ok, _, _} = DBConnection.prepare_execute(blocking_db, insert_query, [])
+          Process.send(parent, :continue, [])
+          Process.sleep(20_000)
+        end)
+      end)
+
+      receive do
+        :continue -> :ok
+      end
+
+      assert {:ok, _, _} = DBConnection.prepare_execute(db, insert_query, [])
+    after
+      File.rm!(file)
+    end
   end
 end
