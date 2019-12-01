0be625b65813c7dfa2f528a2d0001a06a06aa3fa
Moritz Ulrich <moritz@tarn-vedra.de>
2019-12-01 22:38:22 +0100

    CI: Set lower  minimal elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Exqlite.MixProject do
     [
       app: :exqlite,
       version: "0.1.0",
-      elixir: "~> 1.9",
+      elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
