047fbc569906c361de4ea952d4bb92e2972c3cf3
Moritz Ulrich <moritz@tarn-vedra.de>
2019-12-14 12:12:30 +0100

    Start working on the top-level module

--- lib/exqlite.ex
+++ lib/exqlite.ex
@@ -1,3 +1,108 @@
 defmodule Exqlite do
+  @moduledoc """
+  Sqlite3 driver for Elixir.
+  """
+
+  @type start_option() ::
+    {:database, binary()}
+    | DBConnection.start_option()
+
+  @type option() :: DBConnection.option()
+
+  @doc """
+  Opens a connection to a SQLite database.
+
+  ## Examples
+
+  Start a in-memory database connection
+
+      iex> {:ok, db} = Exqlite.start_link([database: ':memory:'])
+      iex> is_pid(db)
+      true
+
+  Start a database connection to a file
+
+      iex> file = Path.absname("test.db")
+      iex> {:ok, _db} = Exqlite.start_link([database: file])
+      iex> File.exists?(file)
+      true
+
+  """
+  @spec start_link([start_option()]) :: GenServer.on_start
+  def start_link(opts) do
+    DBConnection.start_link(Exqlite.Connection, opts)
+  end
+
+  @doc """
+  Run queries against a given database connection.
+
+  ## Options
+
+  Options are passed to `DBConnection.execute/4` for text protocol, and
+  `DBConnection.prepare_execute/4` for binary protocol. See their documentation
+  for all available options.
+
+  ## Examples
+
+  Run queries
+
+      iex> {:ok, db} = Exqlite.start_link([database: ':memory:'])
+      iex> {:ok, _} = Exqlite.query(db, "create table test (number integer)")
+      iex> {:ok, _} = Exqlite.query(db, "insert into test (number) values (42)")
+      iex> {:ok, result} = Exqlite.query(db, "select * from test")
+      iex> result.rows
+      [[number: 42]]
+
+  """
+  def query(conn, statement, params \\ [], options \\ []) do
+    query = %Exqlite.Query{statement: statement}
+    DBConnection.prepare_execute(conn, query, params, options)
+    |> query_result()
+  end
+
+  defp query_result({:ok, _query, result}), do: {:ok, result}
+  defp query_result({:error, _} = error), do: error
+
+  def prepare(conn, statement, options \\ []) do
+    query = %Exqlite.Query{statement: statement}
+    DBConnection.prepare(conn, query, options)
+  end
+
+  def prepare_execute(conn, statement, options \\ []) do
+    query = %Exqlite.Query{statement: statement}
+    DBConnection.prepare_execute(conn, query, options)
+  end
+
+  defdelegate execute(conn, query, params, opts \\ []), to: DBConnection
+
+  def close(conn, %Exqlite.Query{} = query, opts \\ []) do
+    case DBConnection.close(conn, query, opts) do
+      {:ok, _} ->
+        :ok
+
+      {:error, _} = error ->
+        error
+    end
+  end
+
+  defdelegate transaction(conn, fun, opts \\ []), to: DBConnection
+
+  defdelegate rollback(conn, reason), to: DBConnection
+
+  def stream(%DBConnection{} = conn, statement, params, opts) when is_binary(statement) do
+    query = %Exqlite.Query{statement: statement}
+
+    opts = Keyword.put_new(opts, :max_rows, 500)
+    DBConnection.prepare_stream(conn, query, params, opts)
+  end
+
+  def stream(%DBConnection{} = conn, %Exqlite.Query{} = query, params, opts) do
+    opts = Keyword.put_new(opts, :max_rows, 500)
+    DBConnection.stream(conn, query, params, opts)
+  end
+
+  def child_spec(opts) do
+    DBConnection.child_spec(Exqlite.Connection, opts)
+  end
 
 end
--- test/exqlite_test.exs
+++ test/exqlite_test.exs
@@ -2,7 +2,25 @@ defmodule ExqliteTest do
   use ExUnit.Case
   doctest Exqlite
 
-  # test "greets the world" do
-  #   assert Exqlite.hello() == :world
-  # end
+  alias Exqlite.Result
+
+  defp db!() do
+    {:ok, db} = Exqlite.start_link(database: ':memory:')
+    db
+  end
+
+  test "query" do
+    db = db!()
+    assert {:ok, %Result{}} = Exqlite.query(db, "create table test (number integer)")
+    assert {:ok, %Result{rows: []}} = Exqlite.query(db, "select * from test")
+    assert {:ok, %Result{}} = Exqlite.query(db, "insert into test (number) values(?)", [42])
+    assert {:ok, %Result{rows: [[{:number, 42}]]}} = Exqlite.query(db, "select * from test")
+  end
+
+  test "unique violations" do
+    db = db!()
+    assert {:ok, %Result{}} = Exqlite.query(db, "create table test (number integer unique)")
+    assert {:ok, %Result{}} = Exqlite.query(db, "insert into test (number) values(?)", [42])
+    assert {:error, "UNIQUE constraint failed: test.number"} = Exqlite.query(db, "insert into test (number) values(?)", [42])
+  end
 end
